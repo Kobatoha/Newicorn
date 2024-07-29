ccd239cb00c955aba31975c8a362a336f105311f
kobatoha <kobatoha@gmail.com>
2024-07-29 16:11:13 +0300

    update

--- functions/functions.py
+++ functions/functions.py
@@ -19,3 +19,83 @@ def status_bar_message(message):
     if message:
         return message
     return 'status_bar_message'
+
+
+def toggle_f1():
+    print('press toggle_f1')
+
+
+def toggle_f2():
+    print('press toggle_f2')
+
+
+def toggle_f3():
+    print('press toggle_f3')
+
+
+def toggle_f4():
+    print('press toggle_f4')
+
+
+def toggle_f5():
+    print('press toggle_f5')
+
+
+def toggle_q():
+    print('press toggle_q')
+
+
+def toggle_w():
+    print('press toggle_w')
+
+
+def toggle_e():
+    print('press toggle_e')
+
+
+def toggle_r():
+    print('press toggle_r')
+
+
+def toggle_t():
+    print('press toggle_t')
+
+
+def toggle_1():
+    print('press toggle_1')
+
+
+def toggle_2():
+    print('press toggle_2')
+
+
+def toggle_3():
+    print('press toggle_3')
+
+
+def toggle_4():
+    print('press toggle_4')
+
+
+def toggle_5():
+    print('press toggle_5')
+
+
+def toggle_6():
+    print('press toggle_6')
+
+
+def toggle_7():
+    print('press toggle_7')
+
+
+def toggle_8():
+    print('press toggle_8')
+
+
+def toggle_9():
+    print('press toggle_9')
+
+
+def toggle_tilda():
+    print('press toggle_tilda')
