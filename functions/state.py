3fab7ca85f6c663fb9bee2e136ef9bb99ff738a8
kobatoha <kobatoha@gmail.com>
2024-07-29 16:10:47 +0300

    init

---
+++ functions/state.py
@@ -0,0 +1,26 @@
+pressed_f1 = False
+pressed_f2 = False
+pressed_f3 = False
+pressed_f4 = False
+pressed_f5 = False
+
+pressed_q = False
+pressed_w = False
+pressed_e = False
+pressed_r = False
+pressed_t = False
+
+pressed_1 = False
+pressed_2 = False
+pressed_3 = False
+pressed_4 = False
+pressed_5 = False
+
+pressed_6 = False
+pressed_7 = False
+pressed_8 = False
+pressed_9 = False
+pressed_tilda = False
+
+
+
