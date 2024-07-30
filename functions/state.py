0cb30a1a8b9a0b61f9fb3f007e2e5aecb73e778a
kobatoha <kobatoha@gmail.com>
2024-07-30 21:13:12 +0300

    update state status

--- functions/state.py
+++ functions/state.py
@@ -4,11 +4,23 @@ pressed_f3 = False
 pressed_f4 = False
 pressed_f5 = False
 
+pressed_f6 = False
+pressed_f7 = False
+pressed_f8 = False
+pressed_f9 = False
+pressed_f10 = False
+
 pressed_q = False
 pressed_w = False
 pressed_e = False
 pressed_r = False
 pressed_t = False
+
+pressed_y = False
+pressed_u = False
+pressed_i = False
+pressed_o = False
+pressed_p = False
 
 pressed_1 = False
 pressed_2 = False
@@ -20,6 +32,9 @@ pressed_6 = False
 pressed_7 = False
 pressed_8 = False
 pressed_9 = False
+pressed_0 = False
+
 pressed_tilda = False
 
 pressed_res = False
+
