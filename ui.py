841ff22c7a9683389a715d0755d5a87f7c17b9ae
kobatoha <kobatoha@gmail.com>
2024-07-30 21:13:47 +0300

    update start_stop button

--- ui.py
+++ ui.py
@@ -4,7 +4,8 @@ from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVB
 from PyQt6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QMainWindow, QProgressBar, QLineEdit, QLabel, QMenuBar
 from PyQt6.QtGui import QIcon, QPixmap, QFont
 from PyQt6.QtCore import QRect, QSize, Qt
-from uni_widgets import start_stop, lock_window, save_load, status_bar, keys, resurrection
+from uni_widgets import start_stop, lock_window, save_load, status_bar, keys, resurrection, icons_and_lines, profiles
+from functions import state, functions
 
 
 class Unicorn(QMainWindow):
@@ -27,12 +28,15 @@ class Unicorn(QMainWindow):
         layout = QVBoxLayout()
         central_widget.setLayout(layout)
 
-        start_stop.create_start_stop(central_widget)
-        lock_window.create_lock_window(central_widget)
+        hwnd_line_edit = lock_window.create_lock_window(central_widget)
         save_load.create_save_load(central_widget)
-        status_bar.create_status_bar(central_widget)
-        keys.create_keys(central_widget)
+        info_label = status_bar.create_status_bar(central_widget)
         resurrection.create_resurrection(central_widget)
+        icons_and_lines.create_icons_and_lines(central_widget)
+        profiles.create_profile(central_widget)
+        check_boxes = keys.create_keys(central_widget)
+
+        start_stop.create_start_stop(central_widget, [info_label, check_boxes])
 
 
 
