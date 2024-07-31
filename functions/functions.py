b9d7d86afad3875275c1e51f96de8e0880aee9bf
kobatoha <kobatoha@gmail.com>
2024-07-31 23:11:19 +0300

    add in start_stop toggle_key

--- functions/functions.py
+++ functions/functions.py
@@ -1,21 +1,38 @@
 from PyQt6.QtWidgets import QLabel, QLineEdit, QCheckBox
 from PyQt6.QtCore import Qt
-from functions import configuration, state
+from functions import configuration, state, toggles
+import threading
+import keyboard
+
+
+def press_insert(start_stop_button):
+    start_stop_button.click()
+
+
+def hotkey_thread_insert(self):
+    keyboard.add_hotkey('INSERT', self.press_insert)
 
 
 def start_stop(button, ui_elements):
     info_label = ui_elements[0]
     check_boxes = ui_elements[1]
+    line_edits = ui_elements[2]
+    num = 0
     if button.text() == 'Start':
         print('start')
         state.start = True
         button.setText('Stop')
         info_label.setText("Started clicker")
         for check_box in check_boxes:
+            line_edit = line_edits[num]
             key = check_box.text().lower()
             box_state = check_box.checkState()
             if box_state == Qt.CheckState.Checked:
                 state.state_press[key] = True
+                toggles.toggle_key(key, check_box, line_edit)
+
+            num += 1
+
 
     else:
         print('stop')
