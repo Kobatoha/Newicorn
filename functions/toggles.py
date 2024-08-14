9dc566860d5b2fcc859c5c603f878af8521259be
kobatoha <kobatoha@gmail.com>
2024-08-14 18:53:02 +0300

    small fixes

--- functions/toggles.py
+++ functions/toggles.py
@@ -16,19 +16,20 @@ def toggle_key(key, check_box, line_edit):
             print('State start:', state.start)
             state.state_press[key] = True
             # press_key(key, line_edit)
-            threading.Thread(target=press_key(key, line_edit)).start()
+            threading.Thread(target=press_key(line_edit=line_edit, key=key)).start()
     else:
         state.state_press[key] = False
     print(f'press toggle_{key}')
 
 
-def press_key(key, line_edit):
-    while state.state_press[key]:
-        interval = int(line_edit.text())
-        print(key, configuration.down, configuration.keys_num[key], 0)
-        time.sleep(interval / 1000)
-        print(key, configuration.up, configuration.keys_num[key], 0)
-        QTimer.singleShot(interval, press_key)
+def press_key(line_edit=None, key=None):
+    if key and line_edit:
+        while state.state_press[key]:
+            interval = int(line_edit.text())
+            print(key, configuration.down, configuration.keys_num[key], 0)
+            time.sleep(interval / 1000)
+            print(key, configuration.up, configuration.keys_num[key], 0)
+            QTimer.singleShot(interval, press_key)
 
 
 def toggle_res(state):
